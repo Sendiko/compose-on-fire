c5075002ad77f475a70f79f7df5f5b2bd62c1e9d
Sendiko <sarangtawon897@gmail.com>
2024-07-04 13:17:21 +0700

    import theme and dependencies

---
+++ app/src/main/java/com/sendiko/composeonfire/signin/presentation/SignInScreen.kt
@@ -0,0 +1,22 @@
+package com.sendiko.composeonfire.signin.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SignInScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold {
+        Column(
+            modifier = Modifier.padding(it),
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
+    }
+}
