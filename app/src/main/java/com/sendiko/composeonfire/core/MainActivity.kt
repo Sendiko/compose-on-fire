f319628ad2ef8e159045890c97a76276ac2e0adc
Sendiko <sarangtawon897@gmail.com>
2024-07-06 10:37:28 +0700

    try sign in screen

--- app/src/main/java/com/sendiko/composeonfire/core/MainActivity.kt
+++ app/src/main/java/com/sendiko/composeonfire/core/MainActivity.kt
@@ -2,46 +2,61 @@ package com.sendiko.composeonfire.core
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sendiko.composeonfire.core.ui.theme.ComposeOnFireTheme
+import com.sendiko.composeonfire.signin.presentation.SignInScreen
+import com.sendiko.composeonfire.signin.presentation.SignInScreenViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposeOnFireTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel = viewModel<SignInScreenViewModel>()
+                val state by viewModel.state.collectAsState()
+
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult(),
+                    onResult = { result ->
+                        if (result.resultCode == RESULT_OK){
+                            lifecycleScope.launch {
+                                val signInResult = viewModel.signInWithIntent(
+                                    intent = result.data ?: return@launch
+                                )
+                                viewModel.onSignInResult(signInResult)
+                            }
+                        }
+                    }
+                )
+
+                SignInScreen(
+                    state = state,
+                    onSignInClick = {
+                        viewModel.setLoadingState(true)
+                        lifecycleScope.launch {
+                            val signInIntentSender = viewModel.signIn()
+                            launcher.launch(
+                                IntentSenderRequest.Builder(
+                                    signInIntentSender ?: return@launch
+                                ).build()
+                            )
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeOnFireTheme {
-        Greeting("Android")
     }
 }
